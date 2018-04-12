e197788371c65e4a6890e3b55d3df3ec14c1fdd4
Dezarnaud Antoine <antoine.dezarnaud@etu.unice.fr>
2018-04-12 11:26:54 +0200

    get rid of commands

--- src/consumer/client/Client.java
+++ src/consumer/client/Client.java
@@ -7,49 +7,13 @@ import common.DistantStreamReader;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.Scanner;
 
 public class Client implements DistantObjectObserver{
 
     private DistantObject objetDistant = null;
-    private int consumerPort;
 
-    public void launch(int numPort) {
-
-        this.consumerPort = numPort;
-        //Listening keyboard
-        Scanner scanner = new Scanner(System.in);
-        String cmdLine;
-        String cmd;
-        do{
-            cmd = scanner.next();
-            cmdLine = scanner.nextLine().trim();
-            String[] cmdArgs = cmdLine.split(" ");
-            doActionForCommand(cmd, cmdArgs);
-
-        }while(!cmd.equals("quit"));
-    }
-
-    private void doActionForCommand(String cmd, String[] args) {
-
-        switch (cmd){
-            case "subscribe":
-                String producerPort = (args.length>0? args[0] : "");
-                if(producerPort.isEmpty()){
-                    System.out.println("You need to specify a port number to reach the producer server.");
-                    displayHelp();
-                }else{
-                    connectToProducerServer(args[0]);
-                }
-                break;
-            case "quit":
-                System.out.println("Bye :)");
-                break;
-            case "help":
-            default:
-                displayHelp();
-                break;
-        }
+    public void launch() {
+        connectToProducerServer("2501");
     }
 
     private void connectToProducerServer(String portNumber) {
@@ -65,18 +29,11 @@ public class Client implements DistantObjectObserver{
             objetDistant.addObserver(this);
             String registerName = "client";
             r.rebind(registerName,objetDistant);
-            System.out.println("Object sended on port "+ consumerPort +" with name \""+registerName+"\". The distant server can now send data anytime.");
+            System.out.println("Object sended on port "+ 2501 +" with name \""+registerName+"\". The distant server can now send data anytime.");
         } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
-
-    private void displayHelp() {
-        System.out.println("Help :\n" +
-                "subscribe [producer_server_port]\n Subscribe to the producer server giving the remote object.\n" +
-                "help\n Display this help message.\n");
-    }
-
 
     @Override
     public void update(DistantObject observable, String updateMsg) throws RemoteException {
--- src/consumer/client/ClientLauncher.java
+++ src/consumer/client/ClientLauncher.java
@@ -2,7 +2,6 @@ package consumer.client;
 
 public class ClientLauncher {
     public static void main(String[] args) {
-        int numPort = (args.length>0? Integer.parseInt(args[0]) : 2500);
-        new Client().launch(numPort);
+        new Client().launch();
     }
 }
